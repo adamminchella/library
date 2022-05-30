9a802b5fb977f130cb53bf24d15bfa134c4d7a29
Adam Minchella <adamminchella@msn.com>
2022-05-30 14:43:31 +0100

    Add function which displays library in a table

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tableBody = document.querySelector(".table-body");
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -5,13 +7,29 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.read = isRead;
-  this.info = function () {
-    let text = this.read ? "has been read" : "has not been read";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${text}`;
-  };
+  //   this.info = function () {
+  //     let text = this.read ? "has been read" : "has not been read";
+  //     return `${this.title} by ${this.author}, ${this.pages} pages, ${text}`;
+  //   };
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
   let newBook = new Book(title, author, pages, isRead);
   myLibrary.push(newBook);
 }
+
+addBookToLibrary("Harry Potter", "J.K. Rowling", 624, true);
+
+function displayLibrary() {
+  for (let book of myLibrary) {
+    const tableRow = document.createElement("tr");
+    for (let key in book) {
+      const tableCell = document.createElement("td");
+      tableCell.textContent = book[key];
+      tableRow.appendChild(tableCell);
+    }
+    tableBody.appendChild(tableRow);
+  }
+}
+
+displayLibrary();
